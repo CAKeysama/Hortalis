22b1067ae817f0f7d1ba165979b9fa6d3f90ccb8
Gustavo D. Silva <104632552+CAKeysama@users.noreply.github.com>
2025-05-28 08:14:31 -0300

    Fix tutorials page build configuration

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     open: true
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        tutoriais: './tutoriais.html'
-      }
-    }
   }
 });
